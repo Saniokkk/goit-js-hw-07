74e4af8ecf47611a9c699b383505f3badf28fc6b
Aleksandr.Kashirin <saniok26@rambler.ru>
2022-01-18 00:32:50 +0200

    task-1-good

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,35 +2,50 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
-// Выполняй это задание в файлах 01-gallery.html и 01-gallery.js. Разбей его на несколько подзадач:
-
-// Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.
-// Реализация делегирования на div.gallery и получение url большого изображения.
-// Подключение скрипта и стилей библиотеки модального окна basicLightbox. Используй CDN сервис jsdelivr и добавь в проект ссылки на минифицированные (.min) файлы библиотеки.
-// Открытие модального окна по клику на элементе галереи. Для этого ознакомься с документацией и примерами.
-// Замена значения атрибута src элемента <img> в модальном окне перед открытием. Используй готовую разметку модального окна с изображением из примеров библиотеки basicLightbox.
-
 
 const galleryRef = document.querySelector('.gallery');
-// galleryRef.addEventListener('click', );
-console.log(galleryRef);
 
-function createListGalleryItems() {
+function createListGalleryItems(galleryItems) {
     const listImage = galleryItems.reduce((acc, { preview, original, description }) => {
         acc += 
         `<div class="gallery__item">
-            <a class="gallery__link" href="${original}">
-                <img
-                class="gallery__image"
-                src="${preview}"
-                data-source="${original}"
-                alt="${description}"
-                />
-            </a>
+        <a class="gallery__link" href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+        />
+        </a>
         </div>`
         return acc;
-    }, '');
-    return listImage;
+    }, ''); 
+    galleryRef.insertAdjacentHTML('beforeend', listImage);
 };
-galleryRef.insertAdjacentHTML('beforeend', createListGalleryItems());
-// console.log(createListGalleryItems(galleryItems));
+createListGalleryItems(galleryItems);
+
+galleryRef.addEventListener('click', onOpenImage);
+
+function onOpenImage(event) {
+    event.preventDefault();
+
+    if (event.target.nodeName !== "IMG") {
+    return;
+    }
+    const url = event.target.dataset.source;
+    const imgOnModal = basicLightbox.create(`
+    <img width="1280" height="854" src="${url}"/>`);
+
+    imgOnModal.show();    
+    
+    document.addEventListener('keydown', closeModalEsc);
+
+    function closeModalEsc(event) {
+        if (event.code !== 'Escape') {
+            return;
+        };
+        imgOnModal.close();
+    }
+}
+
+
