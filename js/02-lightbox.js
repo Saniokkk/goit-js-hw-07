d961dadce4c35bf44de0a0abdd393c1947eb0876
Aleksandr.Kashirin <saniok26@rambler.ru>
2022-01-20 23:39:46 +0200

    ready

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,24 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+
+function createListGalleryItems(galleryItems) {
+    const listImage = galleryItems.reduce((acc, { preview, original, description }) => {
+        acc += 
+        `<a class="gallery__item" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+        `
+        return acc;
+    }, ''); 
+    galleryRef.insertAdjacentHTML('beforeend', listImage);
+};
+createListGalleryItems(galleryItems);
+
+new SimpleLightbox('.gallery__item',
+    {
+captionDelay: 250,
+captionsData: "alt",        
+    });
